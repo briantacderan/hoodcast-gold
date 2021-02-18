c5ce715df063e5ac395bd8cbd83c95f878e71ff9
Brian Tacderan <briantacderan@gmail.com>
2021-02-18 04:38:02 -0800

    Fix table selector in javascript

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var idx = 0;
   var body = document.querySelector('body');
   var container = document.getElementById('container');
+  var dfName = document.getElementById('balance-selector').value;
 
   body.style.background = 'white';
   container.style.height = '75vh';
@@ -15,26 +16,15 @@ $(document).ready(function() {
       }
     }
   }
-
-  var selector = document.getElementById('balance-selector');
-  var dfName = selector.value;
-  console.log(dfName);
-
-  selector.addEventListener('click', function() {
-    if (selector.value != dfName) {
-      console.log(dfName);
-      $(`#to-${dfName}`).hide();
-      handleChange();
-    }
+    
+  document.querySelectorAll('.selectors').forEach(item => {
+    item.addEventListener('change', function() {
+      if (item.value != dfName) {
+        $(`#to-${dfName}`).hide();
+        dfName = item.value;
+        $(`#to-${dfName}`).show();
+      }
+    });
   });
     
-  selector.addEventListener('change', function() {
-    if (selector.value != dfName) {
-      console.log(dfName);
-      $(`#to-${dfName}`).hide();
-        
-      dfName = selector.value;
-      $(`#to-${dfName}`).show();
-    }
-  });
 });
